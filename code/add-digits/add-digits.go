a72df2ca19ad0b534ba7381d779983555f00582d
tsunami42 <sunnemin@gmail.com>
2016-07-04 23:55:38 +0800

    edge case

--- code/add-digits/add-digits.go
+++ code/add-digits/add-digits.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(addDigits(38))
+	fmt.Println(addDigits(10))
 }
 
+//	case: 10
 func addDigits(num int) int {
-	for num > 10 {
+	for num >= 10 {
 		num = num%10 + num/10
 	}
 	return num
